c08ca48f73bed479eb3e7c302806fbacad7c8114
Ivan <luckybn310@gmail.com>
2023-09-11 21:48:56 +0630

    final commit

--- src/layout/SideBar.tsx
+++ src/layout/SideBar.tsx
@@ -14,7 +14,7 @@ function SideBar({openNav}: TSideBarProps) {
   const [ isFold, setIsFold ] = useState(false)
 
   return (
-    <div className={`absolute z-10 top-24 md:top-0 md:relative opacity-0 md:opacity-100 ${openNav ? " scale-y-100 opacity-100" : "scale-y-90"} origin-top bg-white  rounded-2xl shadow-lg md:shadow-sm  group py-4 ${isFold ? "w-[72px] px-3" : "w-60 px-5"} transition-all duration-300 ease-in-out`}>
+    <div className={`absolute z-10 top-24 md:top-0 md:relative opacity-0 md:opacity-100 ${openNav ? " scale-y-100 opacity-100" : "scale-y-90"} md:scale-y-100 origin-top bg-white  rounded-2xl shadow-lg md:shadow-sm  group py-4 ${isFold ? "w-[72px] px-3" : "w-60 px-5"} transition-all duration-300 ease-in-out`}>
       <div className={` absolute top-12 -right-4 bg-violet-500 rounded-lg cursor-pointer opacity-0 group-hover:opacity-100 transition-all duration-300 ease-out ${isFold && "rotate-180"}`}>
         <Icon icon={ChevronLeftIcon} onClick={() => setIsFold(!isFold)} className=" hidden md:block text-white"/>
       </div>
